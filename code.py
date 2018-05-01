632effa4ea8ab35af0b3f7f2a9020c39b2d16bd9
Aaron Guidarelli <guidarela2@students.rowan.edu>
2018-05-01 12:59:52 +0000

    Change architecture to VGG-A, other stuff I think

--- code.py
+++ code.py
@@ -30,6 +30,8 @@ from keras.models import Sequential
 from keras.layers import Dropout, Flatten, Dense, Conv2D, MaxPooling2D
 from keras.callbacks import ModelCheckpoint
 from keras import applications
+from keras import optimizers
+from keras import initializers
 import os
 
 # dimensions of our images.
@@ -41,7 +43,7 @@ validation_data_dir = 'data/validation'
 nb_train_samples = 1080*85
 nb_validation_samples = 120*85
 epochs = 100
-batch_size = 60
+batch_size = 120
 nb_nodes = 4096
 nb_nodes_last = 1000
 nb_nodes_small_factor = 2
@@ -80,49 +82,52 @@ def trainSimpleVgg():
 	
 	#first conv layer
 	vggInspired.add(Conv2D(64, kernel_size=3, strides=1, input_shape=inputShape, padding='same', activation='relu'))
-	vggInspired.add(Conv2D(64, kernel_size=3, strides=1, padding='same', activation='relu'))
+	#vggInspired.add(Conv2D(64, kernel_size=3, strides=1, padding='same', activation='relu'))
 	vggInspired.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
 	
 	#second conv layer
 	vggInspired.add(Conv2D(128, kernel_size=3, strides=1, padding='same', activation='relu'))
-	vggInspired.add(Conv2D(128, kernel_size=3, strides=1, padding='same', activation='relu'))
+	#vggInspired.add(Conv2D(128, kernel_size=3, strides=1, padding='same', activation='relu'))
 	vggInspired.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
 	
 	#third conv layer
 	vggInspired.add(Conv2D(256, kernel_size=3, strides=1, padding='same', activation='relu'))
 	vggInspired.add(Conv2D(256, kernel_size=3, strides=1, padding='same', activation='relu'))
-	vggInspired.add(Conv2D(256, kernel_size=3, strides=1, padding='same', activation='relu'))
+	#vggInspired.add(Conv2D(256, kernel_size=3, strides=1, padding='same', activation='relu'))
 	vggInspired.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
 	
 	#fourth conv layer
 	vggInspired.add(Conv2D(512, kernel_size=3, strides=1, padding='same', activation='relu'))
 	vggInspired.add(Conv2D(512, kernel_size=3, strides=1, padding='same', activation='relu'))
-	vggInspired.add(Conv2D(512, kernel_size=3, strides=1, padding='same', activation='relu'))
+	#vggInspired.add(Conv2D(512, kernel_size=3, strides=1, padding='same', activation='relu'))
 	vggInspired.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
 	
 	#fifth conv layer
 	vggInspired.add(Conv2D(512, kernel_size=3, strides=1, padding='same', activation='relu'))
 	vggInspired.add(Conv2D(512, kernel_size=3, strides=1, padding='same', activation='relu'))
-	vggInspired.add(Conv2D(512, kernel_size=3, strides=1, padding='same', activation='relu'))
+	#vggInspired.add(Conv2D(512, kernel_size=3, strides=1, padding='same', activation='relu'))
 	vggInspired.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
 	
 	#flatten
 	vggInspired.add(Flatten())
 	
+	randnorm = initializers.RandomNormal(mean=0.0, stddev=0.1)
+
 	#fc layers
-	vggInspired.add(Dense(nb_nodes // nb_nodes_small_factor))
+	vggInspired.add(Dense(nb_nodes // nb_nodes_small_factor, kernel_initializer=randnorm))
 	vggInspired.add(Dropout(.5))
-	vggInspired.add(Dense(nb_nodes // nb_nodes_small_factor))
+	vggInspired.add(Dense(nb_nodes // nb_nodes_small_factor, kernel_initializer=randnorm))
 	vggInspired.add(Dropout(.5))
-	vggInspired.add(Dense(nb_nodes_last // nb_nodes_small_factor))
+	#vggInspired.add(Dense(nb_nodes_last // nb_nodes_small_factor))
 	
 	#output softmax
 	vggInspired.add(Dense(nb_classes, activation='softmax'))
 	
 	vggInspired.summary()
-	
+
+	sgd = optimizers.SGD(lr=0.01, momentum=0.9)
 	# set optmizer and compile model
-	vggInspired.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=["accuracy"])
+	vggInspired.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=["accuracy"])
 	
 	# Prepare model model saving directory.
 	save_dir = os.path.join(os.getcwd(), 'saved_models')
